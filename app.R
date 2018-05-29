a96e833de8e0492df6354c63f9281a6812ad0ff0
Patrick M Groden <pmg67@uw.edu>
2018-05-29 16:45:30 -0700

    Joined parts 1 and 3

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(plotly)
+# install.packages("shinythemes")
 
 underage_prison_data <- read.csv('data/QT_less than 18 year olds_total.csv', 
                                  stringsAsFactors = FALSE)
@@ -44,6 +45,109 @@ long_underage_data <- left_join(long_underage_data, long_male_data)
 
 year_range <- range(long_underage_data$year)
 
+# Matt's data for Tab 1 #
+
+prison_total <- read.csv("data/final_imprison_total_csv.csv",
+                         stringsAsFactors = FALSE
+)
+
+prison_rate <- read.csv("data/final_imprison_rate_csv.csv",
+                        stringsAsFactors = FALSE
+)
+
+colnames(prison_total) <- c("Jurisdiction", 1978:2016)
+
+colnames(prison_rate) <- c("Jurisdiction", 1978:2016)
+
+
+prison_total_long <- gather(prison_total,
+                            key = year, value = pop, "1978", "1979", "1980", "1981", "1982", "1983",
+                            "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992",
+                            "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001",
+                            "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+                            "2010", "2011", "2012", "2013", "2014", "2015", "2016"
+)
+
+prison_total_long$pop <- as.numeric(prison_total_long$pop)
+
+prison_total_long$pop <- as.numeric(prison_total_long$pop)
+
+prison_rate_long <- gather(prison_rate,
+                           key = year, value = pop, "1978", "1979", "1980", "1981", "1982", "1983",
+                           "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992",
+                           "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001",
+                           "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+                           "2010", "2011", "2012", "2013", "2014", "2015", "2016"
+)
+
+prison_rate_long$pop <- as.numeric(prison_rate_long$pop)
+
+prison_rate_national <- filter(prison_rate_long, Jurisdiction == "U.S. total")
+
+prison_rate_non_national <- filter(
+  prison_rate_long,
+  Jurisdiction != "Federal" & Jurisdiction != "State" &
+    Jurisdiction != "U.S. total"
+)
+
+prison_total_point <- ggplot(data = prison_total_long) +
+  geom_point(aes(x = year, y = pop, color = Jurisdiction))
+
+prison_rate_point <- ggplot(data = prison_rate_long) +
+  geom_point(aes(x = year, y = pop, color = Jurisdiction))
+
+prison_rate_national_point <- ggplot(data = prison_rate_national) +
+  geom_point(aes(x = year, y = pop, color = Jurisdiction))
+
+prison_rate_non_national_point <- ggplot(data = prison_rate_non_national) +
+  geom_point(aes(x = year, y = pop, color = Jurisdiction))
+
+prison_total_national <- filter(
+  prison_total_long,
+  Jurisdiction == "U.S. total"
+)
+
+prison_total_national_point <- ggplot(data = prison_total_national) +
+  geom_point(aes(x = year, y = pop, color = Jurisdiction))
+
+state_map <- map_data("state")
+
+prison_total_non_national <- filter(
+  prison_total_long,
+  Jurisdiction != "Federal" &
+    Jurisdiction != "State"
+)
+
+prison_states_only <- filter(prison_total_long, Jurisdiction != "Federal"
+                             & Jurisdiction != "State" & Jurisdiction != "U.S. total")
+
+prison_states_only_lowercase <-
+  sapply(prison_states_only$Jurisdiction, tolower)
+
+prison_total_states_only <- mutate(prison_states_only,
+                                   region = prison_states_only_lowercase
+)
+
+prison_state_map <- left_join(prison_total_states_only,
+                              state_map,
+                              by = "region"
+)
+
+prison_rate_non_national_lowercase <-
+  sapply(prison_rate_non_national$Jurisdiction, tolower)
+
+prison_rate_non_national <- mutate(prison_rate_non_national,
+                                   region = prison_rate_non_national_lowercase
+)
+
+prison_rate_map <- left_join(prison_rate_non_national,
+                             state_map,
+                             by = "region"
+)
+
+###
+
+
 shinyApp(
   ui = tagList(
     navbarPage(
@@ -52,12 +156,26 @@ shinyApp(
       # First tab
       tabPanel("Prison Populations and Rates",
                sidebarPanel(
-                #widget/sidebar here
+                 selectInput("button",
+                             label = "Years to Examine:",
+                             choices = c(1978:2016)
+                 ),
+                 
+                 checkboxGroupInput("checks",
+                                    label = "Jurisdiction",
+                                    choices =
+                                      sort(unique(prison_total_non_national$Jurisdiction)),
+                                    selected = "Alabama"
+                 )
                ),
                mainPanel(
                  tabsetPanel(
-                   tabPanel("Tab 1", "This panel is intentionally left blank"),
-                   tabPanel("Tab 2", "This panel is intentionally left blank"),
+                   tabPanel("Prison Populations by State", h4("Prison Populations by State, 1978-2016"), plotOutput("map1"),
+                            plotlyOutput("chart1"),
+                            plotlyOutput("chart3")),
+                   tabPanel("Prison Rates by State", h4("Prison Rates by State. 1978-2016"), plotOutput("map2"),
+                            plotlyOutput("chart2"),
+                            plotlyOutput("chart4")),
                    tabPanel("Tab 3", "This panel is intentionally left blank")
                  )
                )
@@ -77,7 +195,7 @@ shinyApp(
       tabPanel("Underage Inmate Population", 
                sidebarPanel(
                  selectInput('location_choice', label = "Select a location:", 
-                             long_underage_data$Jurisdiction, selected = "Alabama", 
+                             long_underage_data$Jurisdiction, selected = "U.S. total", 
                              multiple = FALSE, selectize = TRUE),
                  sliderInput('year_choice', label="Select a range of years", min=year_range[1],
                              max=year_range[2], value=year_range, step = 1)
@@ -140,5 +258,108 @@ shinyApp(
                     input$year_choice[2], ". Overall, the total underage inmate population
                   in the US has gradually decreased over time, "))
     })
+    
+    # Matt's server info for Tab 1 #
+    
+    output$map1 <- renderPlot({
+      prison_total_filter_map <- filter(prison_state_map, year == input$button)
+      
+      prison_map_total_year <- ggplot(data = prison_total_filter_map) +
+        geom_polygon(aes(
+          x = long, y = lat,
+          group = group, fill = pop
+        )) + scale_fill_continuous(low = "pink", high = "red") + theme_bw() +
+        theme(axis.ticks = element_blank(),
+              axis.text.x = element_blank(),
+              axis.text.y = element_blank(),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank(),
+              panel.border = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.grid.major = element_blank())
+      
+      return(prison_map_total_year)
+    })
+    
+    output$map2 <- renderPlot({
+      prison_rate_filter_map <- filter(prison_rate_map, year == input$button)
+      
+      prison_map_rate_year <- ggplot(data = prison_rate_filter_map) +
+        geom_polygon(aes(
+          x = long, y = lat,
+          group = group, fill = pop
+        )) + scale_fill_continuous(low = "grey", high = "blue") + theme_bw() +
+        theme(axis.ticks = element_blank(),
+              axis.text.x = element_blank(),
+              axis.text.y = element_blank(),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank(),
+              panel.border = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.grid.major = element_blank())
+      
+      return(prison_map_rate_year)
+    })
+    
+    output$chart1 <- renderPlotly({
+      prison_total_non_national_filter <- filter(
+        prison_total_non_national,
+        Jurisdiction %in% input$checks
+      )
+      
+      prison_total_national_point <- ggplotly(ggplot(
+        data = prison_total_non_national_filter
+      ) +
+        geom_point(aes(x = year, y = pop, color = Jurisdiction)) + labs(
+          title = "Prison Population, 1978-2016",
+          x = "Year",
+          y = "Prison Population",
+          color = "Jurisdiction"
+        ))
+      
+      return(prison_total_national_point)
+    })
+    
+    output$chart2 <- renderPlotly({
+      prison_rate_national_filter <-
+        filter(prison_rate_long, Jurisdiction %in% input$checks)
+      
+      prison_rate_national_point <- ggplotly(
+        ggplot(data = prison_rate_national_filter) +
+          geom_point(aes(x = year, y = pop, color = Jurisdiction)) + labs(
+            title = "Prison Rate, 1978-2016",
+            x = "Year",
+            y = "Prisoners per 100,000 State Residents",
+            color = "Jurisdiction"
+          )
+      )
+      
+      return(prison_rate_national_point)
+    })
+    
+    output$chart3 <- renderPlotly({
+      prison_total_national_point <- ggplotly(ggplot(data = prison_total_national) +
+                                                geom_point(aes(x = year, y = pop)) + labs(
+                                                  title = "National Prison Population, 1978-2016",
+                                                  x = "Year",
+                                                  y = "Prison Population"
+                                                ))
+      
+      return(prison_total_national_point)
+    })
+    
+    output$chart4 <- renderPlotly({
+      prison_rate_national_point <- ggplotly(ggplot(data = prison_rate_national) +
+                                               geom_point(aes(x = year, y = pop)) + labs(
+                                                 title = "National Prison Rate, 1978-2016",
+                                                 x = "Year",
+                                                 y = "Prison Population"
+                                               ))
+      
+      return(prison_rate_national_point)
+    })
+    
+    ###
+    
   }
 )
